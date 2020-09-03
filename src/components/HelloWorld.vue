8b9f38d8df74a54ac0b038f4fae81eda04b0f8e1
qinkaiyuan <qinkaiyuan94@163.com>
2020-09-03 08:57:37 +0800

    学习使用webpack搭建项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="qky1">{{ hello }}
+      <div class="qky2"></div>
+    </div>
+
+    <img src="../static/bluead.png"/>
+
+  </div>
+</template>
+
+<script lang="ts">
+import {Vue, Component,} from 'vue-property-decorator'
+
+@Component
+export default class HelloWorldVue extends Vue {
+  hello = 'hello boy'
+
+  created(){
+    const symbol5 = Symbol.for('42');
+    const symbol6 = Symbol.for('42');
+    const symbol3 = Symbol('foo');
+
+    console.log(symbol5 )
+    console.log(symbol5 == symbol6)
+  }
+}
+</script>
